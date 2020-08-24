4530e0f25e3b1f5a7ed43ee495af1c77494cd4ec
isys35 <isys35@mail.ru>
2020-08-24 15:56:44 +0300

    debug

--- aptekamos.py
+++ aptekamos.py
@@ -19,7 +19,9 @@ from typing import Iterator
 from requests import Response
 import traceback
 from proxy import Proxy
-from requests.exceptions import ProxyError,ConnectTimeout,SSLError
+from requests.exceptions import ProxyError, ConnectTimeout, SSLError
+import re
+import math
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'
@@ -36,6 +38,7 @@ class SearchPhrase:
 class Parse:
     def __init__(self, response_text):
         self.response_text = response_text
+        self.soup = BeautifulSoup(self.response_text, 'lxml')
 
     def parse_header_aptek(self) -> Union[str, None]:
         soup = BeautifulSoup(self.response_text, 'lxml')
@@ -127,6 +130,33 @@ class Parse:
             image_url = image_url['src']
         return descriptions, image_url
 
+    def parse_med_name(self) -> str:
+        block_name = self.soup.find('h1', attrs={'itemprop': 'name'})
+        med_name = block_name.text.replace('\n', ' ').strip()
+        return med_name
+
+    def parse_max_page_aptek(self) -> int:
+        paginator = self.soup.select_one('#d-table-page-num')
+        if not paginator:
+            return 1
+        paginator_text = self.soup.select_one('#d-table-pager-text').text
+        find_number = re.findall('\d+', paginator_text)
+        return math.ceil(max([int(el) for el in find_number])/250)
+
+
+    def parse_aptek_urls_and_prices(self) -> list:
+        apteka_data_blocks = self.soup.select('.org-data')
+        aptek_urls_and_prices = []
+        for apteka_data_block in apteka_data_blocks:
+            tds = apteka_data_block.select('td')
+            url_aptek = tds[2].select('a')[0]['href']
+            price = tds[6].select_one('.ret-drug-price').text.replace('\n', '').strip().replace(u'\xa0', u'')
+            if price == 'показать цену':
+                price = tds[6].select_one('.ret-drug-price')['data-err-price'].replace(u'\xa0', u'').replace('руб.', '').strip()
+            price = float(price)
+            aptek_urls_and_prices.append([url_aptek, price])
+        return aptek_urls_and_prices
+
 
 class AptekamosParser(Parser):
     POST_URL = 'https://aptekamos.ru/Services/WOrgs/getOrgPrice4?compressOutput=1'
@@ -163,9 +193,9 @@ class AptekamosParser(Parser):
                 while True:
                     try:
                         apteka_response = requests.get(apteks_urls[apteka_response_index],
-                                                        headers=HEADERS,
-                                                        proxies=self.proxies,
-                                                        timeout=3)
+                                                       headers=HEADERS,
+                                                       proxies=self.proxies,
+                                                       timeout=3)
                         break
                     except (ProxyError, ConnectTimeout, SSLError):
                         print('ProxyError')
@@ -238,8 +268,6 @@ class AptekamosParser(Parser):
             self.proxies = self.generator_proxies.get_proxies()
             response = requests.get(response.url, headers=HEADERS, proxies=self.proxies)
             return self._get_meds_from_response(response)
-
-
 
     @border_method_info('Обновление цен...', 'Обновление цен завершено.')
     def update_prices(self):
@@ -314,7 +342,8 @@ class AptekamosParser(Parser):
         all_html_prices = []
         for html_response in html_responses:
             html_prices = self._get_prices_from_response(html_response.text,
-                                                         list_search_phrases_for_get_request[html_responses.index(html_response)])
+                                                         list_search_phrases_for_get_request[
+                                                             html_responses.index(html_response)])
             all_html_prices.append(html_prices)
         return all_html_prices
 
@@ -381,7 +410,6 @@ class AptekamosParser(Parser):
                                                  post_data={"orgId": int(aptek.host_id), "wuserId": 0,
                                                             "searchPhrase": med.name})
                     yield search_phrase
-
 
     @border_method_info('Скачивание картинок и описания...', 'Скачивание картинок и описания завершено.')
     def download_image_and_description(self, meds_info_objects):
@@ -464,16 +492,128 @@ class AptekamosParser(Parser):
         return grequests.map(responses)
 
 
+class AptekamosParserRemake(AptekamosParser):
+    def __init__(self, name_parser: str, file_init_data: str) -> None:
+        super().__init__(name_parser, file_init_data)
+        self.progress = str()
+
+    def update_data(self):
+        self.update_apteks()
+        response = self._get_response(self.host + '/tovary')
+        max_page_in_catalog = Parse(response.text).parse_max_page_in_catalogs()
+        pages_urls = [self.host + '/tovary']
+        pages_urls.extend([f'https://aptekamos.ru/tovary?page={i}' for i in range(2, max_page_in_catalog + 1)])
+        for url_catalog_page in pages_urls:
+            self.progress = f'{pages_urls.index(url_catalog_page)} / {len(pages_urls)}'
+            response_page_catalog = self._get_response(url_catalog_page)
+            meds = self._get_meds_from_response(response_page_catalog)
+            for med in meds:
+                response_med = self._get_response(med.url)
+                if self._is_different_packaging(response_med):
+                    packs = self._get_packs(med.host_id)
+                    for pack in packs:
+                        url_filter_pack = med.url + f'?llat=0.0&llng=0.0&on=&so=0&p={pack}&f=&c=&page=1&min=0&max=0&deliv=0&rsrv=0&sale=0&st=0&r=&me=0&duty=0&mn=0'
+                        response_med_pack = self._get_response(url_filter_pack)
+                        self._update_prices(response_med_pack, med)
+                else:
+                    self._update_prices(response_med, med)
+
+    def update_apteks(self):
+        self.apteks = {}
+        init_apteks_urls = self.load_initial_data()
+        for aptek_url in init_apteks_urls:
+            response = self._get_response(aptek_url)
+            if not response:
+                continue
+            self.apteks[aptek_url] = self._get_aptek(response)
+
+    def _get_aptek(self, response: Response):
+        """Получение аптек из запроса"""
+        header_aptek = Parse(response.text).parse_header_aptek()
+        if not header_aptek:
+            return None
+        aptek_name = str()
+        for name in NAMES_APTEK:
+            if name in header_aptek:
+                aptek_name = name
+                break
+        aptek_address = Parse(response.text).parse_adress_aptek()
+        aptek_url = response.url
+        aptek_id = aptek_url.replace('/ob-apteke', '').split('-')[-1]
+        return Apteka(name=aptek_name,
+                      url=aptek_url,
+                      address=aptek_address,
+                      host=self.host,
+                      host_id=int(aptek_id))
+
+    def _update_prices(self, response: Response, med: Med):
+        print(response.url)
+        parser = Parse(response.text)
+        med_name = parser.parse_med_name()
+        med = Med(name=med_name, url=med.url, host_id=med.host_id)
+        max_page_aptek = parser.parse_max_page_aptek()
+        parsers = [parser]
+        if max_page_aptek > 1:
+            for page in range(2, max_page_aptek + 1):
+                if re.search('&page=', response.url).group(0):
+                    page_url = re.sub('&page=\d+&', f'&page={page}&', response.url)
+                else:
+                    page_url = response.url + f'?llat=0.0&llng=0.0&on=&so=0&p=0&f=0&c=0&page={page}&min=0&max=0&deliv=0&rsrv=0&sale=0&st=0&r=&me=0&duty=0&mn=0'
+                response_page = self._get_response(page_url)
+                parsers.append(Parse(response_page.text))
+        for parser in parsers:
+            aptek_urls_and_prices = parser.parse_aptek_urls_and_prices()
+            for aptek_url, price in aptek_urls_and_prices:
+                if aptek_url in self.apteks:
+                    apteka = self.apteks[aptek_url]
+                    price = Price(apteka=apteka, med=med, rub=price)
+                    db.add_price(price)
+                    db.aptek_update_updtime(apteka)
+                    print(self.progress)
+
+    def _get_packs(self, med_host_id: int) -> list:
+        filter_url = f'https://aptekamos.ru/Services/WProducts/getMedFilters?medId={med_host_id}&regionId=1&compressOutput=1&_={time.time() * 1000}'
+        response_filter = self._get_response(filter_url)
+        response_filter_json = response_filter.json()
+        packs = [pack['pack'] for pack in response_filter_json['packs']]
+        return packs
+
+    @staticmethod
+    def _is_different_packaging(response: Response) -> bool:
+        soup = BeautifulSoup(response.content, 'lxml')
+        block_packaging = soup.select_one('.pack-num.ret-flt-function.function')
+        if block_packaging:
+            return True
+
+    def _get_meds_from_response(self, response: Response):
+        meds = []
+        names_urls_ids_meds = Parse(response.text).parse_names_urls_ids_meds()
+        for name_url_id_med in names_urls_ids_meds:
+            med = Med(name=name_url_id_med['name'], url=name_url_id_med['url'], host_id=name_url_id_med['id'])
+            meds.append(med)
+        return meds
+
+    def _get_response(self, url: str) -> Response:
+        try:
+            response = requests.get(url, headers=HEADERS, proxies=self.proxies)
+        except (ProxyError, ConnectTimeout):
+            self.proxies = self.generator_proxies.get_proxies()
+            return self._get_response(url)
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 403:
+            self.proxies = self.generator_proxies.get_proxies()
+            return self._get_response(url)
+
+
 if __name__ == '__main__':
     NAME_PARSER = 'aptekamos'
     if NAME_PARSER in os.listdir('parsers'):
         parser = Parser().load_object(f'parsers/{NAME_PARSER}')
-        print(len(parser.meds))
     else:
-        parser = AptekamosParser(NAME_PARSER, 'init_data/aptekamos_init_data.txt')
+        parser = AptekamosParserRemake(NAME_PARSER, 'init_data/aptekamos_init_data.txt')
     try:
-        parser.async_update_prices()
+        parser.update_data()
     except Exception as ex:
         print(traceback.format_exc())
         parser.save_object(parser, f'parsers/{parser.name_parser}')
-
